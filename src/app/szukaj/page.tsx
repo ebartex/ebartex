128cdaf93db29af2685d4f0e5c735b070d616974
ebartex <biuro.bartex@gmail.com>
2024-12-03 19:42:30 +0100

    suspen szukaj 2

--- src/app/szukaj/page.tsx
+++ src/app/szukaj/page.tsx
@@ -3,22 +3,66 @@
 import { Suspense } from 'react';
 import Infobar from "@/common/components/infobar";
 import Navbar from "@/common/components/navbar";
-import Wyniki from "./wyniki";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
+type Product = {
+  nazwa: string;
+  // Dodaj inne pola, które mogą być obecne w danych
+};
+
 export default function Szukaj() {
+  const [results, setResults] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+
   const searchParams = useSearchParams();
-  const query = searchParams.get('q') || '';
+  const query = searchParams.get('q') || ''; // Pobranie wartości parametru 'q' z URL
+
+  // Pobieranie danych z API
+  const fetchResults = async (query: string) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`https://bapi.ebartex.pl/products/format5.json?Product-nazwa=?${encodeURIComponent(query)}?`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+      setResults(data.Product);
+
+    } catch {
+      // Opcjonalnie: Dodaj tutaj logowanie błędów dla debugowania
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Obsługuje zmiany zapytania w URL i wywołuje fetchResults
+  useEffect(() => {
+    if (query) {
+      fetchResults(query);
+    }
+  }, [query]); // Dodano `query` jako zależność
 
   return (
-    <>
+    <Suspense fallback={<div>Ładowanie...</div>}>
       <Infobar />
       <Navbar />
+
       <div className="p-4">
-        <Suspense fallback={<div>Ładowanie wyników...</div>}>
-          <Wyniki query={query} />
-        </Suspense>
+        {loading && <p>Loading...</p>}
+        {!loading && results.length > 0 && (
+          <ul>
+            {results.map((result, index) => (
+              <li key={index}>{result.nazwa}</li>
+            ))}
+          </ul>
+        )}
+        {!loading && results.length === 0 && (
+          <p>No results found</p>
+        )}
       </div>
-    </>
+    </Suspense>
   );
 }
