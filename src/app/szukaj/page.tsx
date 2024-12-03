c2916feddc3d09ef94829721849ed3765399837d
ebartex <biuro.bartex@gmail.com>
2024-12-03 22:07:15 +0100

    szukajka

--- src/app/szukaj/page.tsx
+++ src/app/szukaj/page.tsx
@@ -4,9 +4,12 @@ import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Infobar from "@/common/components/infobar";
 import Navbar from "@/common/components/navbar";
-
+import Link from "next/link";
+import Image from "next/image";
 type Product = {
-  nazwa: string;
+  nazwa: string
+  photo_512: string,
+  tw_id: number
   // Dodaj inne pola, które mogą być obecne w danych
 };
 
@@ -42,16 +45,48 @@ function SearchResults() {
   if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="p-4">
-      {results.length > 0 ? (
-        <ul>
+    <div className="flex flex-col md:flex-row">
+      <aside className="hidden md:flex md:w-1/6 md:bg-gray-200 p-4">
+
+      </aside>
+      <main className="flex-1 p-4">
+      <header className="mb-4">
+        <h1 className="text-2xl font-bold">Wyniki wyszukiwania</h1>
+      </header>
+      <section>
+
+        <ul className="space-y-2">
+        {results.length > 0 ? (
+        <div className="bg-white grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-0 w-full">
           {results.map((result, index) => (
-            <li key={index}>{result.nazwa}</li>
+                                    <Link
+                                    key={result.tw_id}
+                                    href={`/products/view/${result.tw_id}/${encodeURIComponent(
+                                        result.nazwa.toLowerCase().replace(/\s+/g, '-')
+                                    )}`}
+                                    className="p-4 border border-slate-200 flex flex-col items-center hover:bg-slate-100 transition-colors"
+                                >
+                                    <Image
+                                        width={100}
+                                        height={100}
+                                        src={result.photo_512 || "https://via.placeholder.com/150"}
+                                        alt={result.nazwa}
+                                        className="w-32 h-32 object-cover mb-4"
+                                    />
+                                    <span className="text-center">{result.nazwa}</span>
+                                    <p className="text-gray-600 mt-2">ID: {result.tw_id}</p>
+                                </Link>            
+
           ))}
-        </ul>
+        </div>
       ) : (
         <p>No results found</p>
       )}
+
+        </ul>
+      </section>
+    </main>
+
     </div>
   );
 }
